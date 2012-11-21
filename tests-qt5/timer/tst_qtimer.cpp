b1ec8bc8b3097a65da170550d69e78cd07bdc3b7
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2012-11-21 04:12:38 +0200

    Add call to setEventDispatcher in tst_QTimer::timerIdPersistsAfterThreadExit()

--- tests-qt5/timer/tst_qtimer.cpp
+++ tests-qt5/timer/tst_qtimer.cpp
@@ -552,6 +552,7 @@ public:
 void tst_QTimer::timerIdPersistsAfterThreadExit()
 {
     TimerIdPersistsAfterThreadExitThread thread;
+    thread.setEventDispatcher(new EventDispatcherLibEv);
     thread.start();
     QVERIFY(thread.wait(30000));
     QCOMPARE(thread.returnValue, 0);
